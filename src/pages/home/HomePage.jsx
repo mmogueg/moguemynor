ace2721f470c63cd0a38cc980b52e16cbcd916a2
mmogue-2021220 <mmogue-2021220@kinal.edu.gt>
2025-02-24 12:03:42 -0600

    se termino homePage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -6,9 +6,6 @@ import figure_1 from '../../assets/imgs/dev-3.png';
 export const HomePage = () => {
   return (
     <section className='app relative overflow-hidden'>
-      {/* <div className='absolute min-h-screen w-full gradient'>
-
-      </div> */}
       <div style={{
         width: '1200px',
         height: '1200px',
@@ -27,12 +24,17 @@ export const HomePage = () => {
         top: "-570px",
         right: "-470px",
       }}></div>
-      <div className='min-h-screen w-full relative max-xl:flex max-xl:flex-col items-center justify-evenly'>
-        <div className='xl:absolute xl:bottom-[40%] xl:left-[50%]'>
-          <div className='w-[500px]'>
-            <h6 className='font-gotham text-[#9f9f9f] uppercase animate-fade-left animate-once animate-duration-1000'>HI, I AM MOGUE, I DEVELOPER</h6>
-            <h1 className='font-monument-bold text-[#7ed957] uppercase text-5xl animate-fade-left animate-once animate-duration-1000 animate-delay-[250ms]'>the perfect PROJECT</h1>
-            <h6 className='font-gotham text-[#9f9f9f] uppercase animate-fade-left animate-once animate-duration-1000 animate-delay-500'>for your business!</h6>
+      <div className='min-h-screen max-xl:pt-32 w-full relative flex max-xl:flex-col items-center justify-between'>
+        <div className='realtive min-w-[50%] max-xl:px-10 max-xl:flex max-xl:flex-col max-xl:justify-evenly max-xl:space-y-28'>
+          <div className='xl:absolute xl:bottom-[40%] xl:left-[50%]'>
+            <div className='xl:w-[600px] max-xl:w-full '>
+              <h6 className='font-gotham text-[#9f9f9f] uppercase animate-fade-left animate-once animate-duration-1000'>HI, I AM MOGUE, I DEVELOPER</h6>
+              <h1 className='font-monument-bold text-[#7ed957] uppercase max-xl:text-4xl text-6xl animate-fade-left animate-once animate-duration-1000 animate-delay-[250ms]'>the perfect PROJECT</h1>
+              <h6 className='font-gotham text-[#9f9f9f] uppercase animate-fade-left animate-once animate-duration-1000 animate-delay-500'>for your business!</h6>
+            </div>
+          </div>
+          <div className='max-xl:w-full xl:absolute xl:right-10 xl:bottom-10 max-xl:text-right'>
+            <h6 className='w-[230px] text-justify font-gotham text-[#9f9f9f] uppercase animate-fade-right animate-once animate-duration-1000 animate-delay-[750ms]'>A programmer who solves problems in the most fun way. :)</h6>
           </div>
         </div>
         <div className='xl:absolute xl:bottom-0 animate-fade-up animate-once animate-duration-1000'>
